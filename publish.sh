bd1cfab40d51e596a6503737a7780dfc4cb3cfa9
Tim Lee <sylee.tim@gmail.com>
2021-04-12 05:10:34 +0100

    labourecon_accept

--- publish.sh
+++ publish.sh
@@ -2,6 +2,8 @@ read -p "Message for git commit > " message
 bundle exec jekyll build
 git add .
 git commit -m "$message"
+git config --global user.email "sylee.tim@gmail.com"
+git config --global user.name "Tim Lee"
 git push origin main
 cd _site
 git add .
